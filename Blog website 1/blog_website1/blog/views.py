00708407d2a7d2a9c74d3af9bea483a502e0f1d5
“parn” <pjisvgreat@gmail.com>
2024-02-12 01:29:56 +0530

    blog_website complited

--- Blog website 1/blog_website1/blog/views.py
+++ Blog website 1/blog_website1/blog/views.py
@@ -1,22 +1,13 @@
 from datetime import date
 from django.shortcuts import render
-posts = [
+all_posts = [
     {
-    "slug":"puss-in-boots",
-    "image": "puss_in_boots.jpg",
-    "author": "Parn Jain",
-    "date": date(2024,2,11),
-    "title": "Puss in Boots",
-    "excerpt": "Pata nahi yar", 
-    "content": "HElloooooooooooooooooooooooooooooooooooo"
-    },
-    {
-        "slug": "programming-is-fun",
-        "image": "coding.jpg",
-        "author": "Maximilian",
-        "date": date(2022, 3, 10),
-        "title": "Programming Is Great!",
-        "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
+        "slug": "Loving-Vincent",
+        "image": "LV.jpg",
+        "author": "Parn Jain",
+        "date": date(2024, 2, 11),
+        "title": "Loving Vincent",
+        "excerpt": "A complete hand-painted 94 minutes  movie , took 6+ years to make!",
         "content": """
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
@@ -32,12 +23,33 @@ posts = [
         """
     },
     {
-        "slug": "into-the-woods",
-        "image": "woods.jpg",
-        "author": "Maximilian",
-        "date": date(2020, 8, 5),
-        "title": "Nature At Its Best",
-        "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
+    "slug":"puss-in-boots",
+    "image": "puss_in_boots.jpg",
+    "author": "Parn Jain",
+    "date": date(2024,2,11),
+    "title": "Puss in Boots",
+    "excerpt": "A brilliant blend of 3D and hand-paint art, a masterpiece of 2.5D animation and storytelling", 
+    "content": """
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        """
+    },
+    {
+        "slug": "Spiderman-into-the-Spiderverse",
+        "image": "SPM.jpg",
+        "author": "Parn Jain",
+        "date": date(2024, 2, 11),
+        "title": "Spiderman into the Spiderverse",
+        "excerpt": " Elevates animation with its revolutionary style, merging comic book artistry with cinematic brilliance to redefine the superhero genre.",
         "content": """
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
@@ -53,14 +65,23 @@ posts = [
         """
     }
 
-
 ]
+
+
+def get_date(post):
+    return post['date']
+
 # Create your views here.
 def home(request):
-    sorted_posts = posts.sort(key=)
-    # latest_posts = posts
-    return render(request,"blog/index.html")
+    sorted_posts =sorted(all_posts,key=get_date)
+    latest_posts = sorted_posts[-3:]
+    return render(request,"blog/index.html",{
+        "posts": latest_posts
+    })
 def posts(request):
-    return render(request,"blog/all-posts.html")
+    return render(request,"blog/all-posts.html",{
+        "all_posts":all_posts
+    })
 def post_details(request,slug):
-    return render(request, "blog/post-detail.html")
+    identified_post = next(post for post in all_posts if post['slug']==slug)
+    return render(request, "blog/post-detail.html",{"post":identified_post})
