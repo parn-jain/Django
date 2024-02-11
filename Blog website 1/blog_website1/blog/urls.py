8712116a2592e77f553e406d1e8bb1deb36de573
“parn” <pjisvgreat@gmail.com>
2024-02-11 23:57:11 +0530

    working on basic blog_website

---
+++ Blog website 1/blog_website1/blog/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from .import views
+urlpatterns = [
+    path('',views.home, name = 'home'),
+    path('posts', views.posts, name = 'posts'),
+    path('posts/<slug:slug>',views.post_details,name = 'post-detail-page')
+
+]
---
+++ Blog website 1/blog_website1/blog/views.py
@@ -0,0 +1,66 @@
+from datetime import date
+from django.shortcuts import render
+posts = [
+    {
+    "slug":"puss-in-boots",
+    "image": "puss_in_boots.jpg",
+    "author": "Parn Jain",
+    "date": date(2024,2,11),
+    "title": "Puss in Boots",
+    "excerpt": "Pata nahi yar", 
+    "content": "HElloooooooooooooooooooooooooooooooooooo"
+    },
+    {
+        "slug": "programming-is-fun",
+        "image": "coding.jpg",
+        "author": "Maximilian",
+        "date": date(2022, 3, 10),
+        "title": "Programming Is Great!",
+        "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
+        "content": """
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        """
+    },
+    {
+        "slug": "into-the-woods",
+        "image": "woods.jpg",
+        "author": "Maximilian",
+        "date": date(2020, 8, 5),
+        "title": "Nature At Its Best",
+        "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
+        "content": """
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        """
+    }
+
+
+]
+# Create your views here.
+def home(request):
+    sorted_posts = posts.sort(key=)
+    # latest_posts = posts
+    return render(request,"blog/index.html")
+def posts(request):
+    return render(request,"blog/all-posts.html")
+def post_details(request,slug):
+    return render(request, "blog/post-detail.html")
