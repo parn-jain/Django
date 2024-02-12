fca8987d4a2aa773fa32d5b10933091cd0ccd75c
“parn” <pjisvgreat@gmail.com>
2024-02-13 02:19:51 +0530

    saved till here

--- Book_store/Book_store/book_outlet/views.py
+++ Book_store/Book_store/book_outlet/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
+from .models import Book
+
 # Create your views here.
+def index(request):
+    books = Book.objects.all()
+    return render(request,"book_outlet/index.html",{
+        "books": books
+    })
+
+def book_details(request,id):
+    # books = Book.objects.all()
+    book = Book.objects.get(pk = id)
+    # identify = next(book for book in books if book['title']==slug)
+    return render(request,"book_outlet/book_detail.html",{
+        # "books":identify
+        "book":book
+
+    })
+
+
