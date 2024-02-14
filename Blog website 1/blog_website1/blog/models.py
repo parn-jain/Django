ede3e5ef7d138acf216dfd75c1f183a493d2bb66
“parn” <pjisvgreat@gmail.com>
2024-02-14 23:34:02 +0530

    SQL database added in blog-website

--- Blog website 1/blog_website1/blog/models.py
+++ Blog website 1/blog_website1/blog/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.core.validators import MinLengthValidator, MaxLengthValidator 
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length = 50, null = True)
+    email = models.EmailField(max_length = 100, null = True)
+
+    def __str__ (self):
+        return self.name
+    
+class Tag(models.Model):
+    caption = models.CharField(max_length = 30)
+
+    def __str__(self): 
+        return self.caption 
+
+class Post(models.Model):
+    title = models.CharField(max_length = 50)
+    excerpt = models.CharField(max_length = 100)
+    image_name = models.CharField(max_length = 30)
+    date = models.DateField(auto_now = True)
+    slug = models.SlugField(unique = True, db_index = True)
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    author = models.ForeignKey(Author, on_delete = models.SET_NULL,related_name = "posts", null = True)
+    tags = models.ManyToManyField(Tag, related_name = "posts")
+
+
+    def data(self):
+        return f"{self.title} {self.date}"
+
+    def __str__(self):
+        return self.data()
+
+
+
+
--- Blog website 1/blog_website1/blog/views.py
+++ Blog website 1/blog_website1/blog/views.py
@@ -1,71 +1,7 @@
 from datetime import date
 from django.shortcuts import render
-all_posts = [
-    {
-        "slug": "Loving-Vincent",
-        "image": "LV.jpg",
-        "author": "Parn Jain",
-        "date": date(2024, 2, 11),
-        "title": "Loving Vincent",
-        "excerpt": "A complete hand-painted 94 minutes  movie , took 6+ years to make!",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+from .models import Post , Author
 
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-    {
-    "slug":"puss-in-boots",
-    "image": "puss_in_boots.jpg",
-    "author": "Parn Jain",
-    "date": date(2024,2,11),
-    "title": "Puss in Boots",
-    "excerpt": "A brilliant blend of 3D and hand-paint art, a masterpiece of 2.5D animation and storytelling", 
-    "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-    {
-        "slug": "Spiderman-into-the-Spiderverse",
-        "image": "SPM.jpg",
-        "author": "Parn Jain",
-        "date": date(2024, 2, 11),
-        "title": "Spiderman into the Spiderverse",
-        "excerpt": " Elevates animation with its revolutionary style, merging comic book artistry with cinematic brilliance to redefine the superhero genre.",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    }
-
-]
 
 
 def get_date(post):
@@ -73,15 +9,17 @@ def get_date(post):
 
 # Create your views here.
 def home(request):
-    sorted_posts =sorted(all_posts,key=get_date)
-    latest_posts = sorted_posts[-3:]
+    latest_posts = Post.objects.all().order_by("-date")[:3]
+    # sorted_posts =sorted(all_posts,key=get_date)
+    # latest_posts = sorted_posts[-3:]
     return render(request,"blog/index.html",{
         "posts": latest_posts
     })
 def posts(request):
     return render(request,"blog/all-posts.html",{
-        "all_posts":all_posts
+        "all_posts":Post.objects.all()
     })
 def post_details(request,slug):
-    identified_post = next(post for post in all_posts if post['slug']==slug)
-    return render(request, "blog/post-detail.html",{"post":identified_post})
+    identified_post = next(post for post in Post.objects.all() if post.slug==slug)
+    identify_author =  identified_post.author
+    return render(request, "blog/post-detail.html",{"post":identified_post, 'author':identify_author})
