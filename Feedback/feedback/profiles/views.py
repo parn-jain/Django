42c36ce3239fa5f328b5c9b15c9513c7150dceb3
“parn” <pjisvgreat@gmail.com>
2024-02-16 04:52:42 +0530

    Day 6 completed

---
+++ Feedback/feedback/profiles/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponseRedirect
+
+# Create your views here.
+
+def store_file(file):
+    with open("temp/image.png","wb+") as dest:
+        for chunk in file.chunks():
+            dest.write(chunk)
+        
+
+
+
+class CreateProfileView(View):
+    def get(self, request):
+        return render(request, "profiles/create_profile.html")
+
+    def post(self, request):
+        store_file(request.FILES["image"])
+        return HttpResponseRedirect("/profiles")
