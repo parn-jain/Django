6780509979dd377ac4eabf080d752ae0403ced5e
“parn” <pjisvgreat@gmail.com>
2024-02-17 01:52:19 +0530

    day 7 competed

--- Feedback/feedback/profiles/views.py
+++ Feedback/feedback/profiles/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
-
+from .forms import ProfileForm
+from .models import UserProfile
+from django.views.generic.edit import CreateView
+from django.views.generic import ListView
 # Create your views here.
+
 
 def store_file(file):
     with open("temp/image.png","wb+") as dest:
@@ -11,11 +15,36 @@ def store_file(file):
         
 
 
+class CreateProfileView(CreateView):
+    template_name = "profiles/create_profile.html"
+    model = UserProfile
+    fields = "__all__"
+    success_url = "/profiles"
 
-class CreateProfileView(View):
-    def get(self, request):
-        return render(request, "profiles/create_profile.html")
 
-    def post(self, request):
-        store_file(request.FILES["image"])
-        return HttpResponseRedirect("/profiles")
+# class CreateProfileView(View):
+#     def get(self, request):
+#         form= ProfileForm()
+#         return render(request, "profiles/create_profile.html",{
+#             "form":form
+#         })
+
+#     def post(self, request):
+#         submitted_form = ProfileForm(request.POST,request.FILES)
+
+#         if submitted_form.is_valid():
+#             # store_file(request.FILES["image"])
+#             profile = UserProfile(image = request.FILES["user_image"]  )
+#             profile.save()
+
+#             return HttpResponseRedirect("/profiles")
+        
+#         return render(request, "profiles/create_profile.html",{
+#             "form":submitted_form
+#         })
+
+
+class ProfileViews(ListView):
+    model= UserProfile
+    template_name = "profiles/user_profiles.html"
+    context_object_name = "all_profiles"
